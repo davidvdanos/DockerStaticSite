384418ba239861a909aaf84eea086e5cdf3024ac
hwarang098tkd <hwarang098tkd@gmail.com>
2024-10-05 20:50:10 +0300

    save

--- matomo.js
+++ matomo.js
@@ -3,7 +3,7 @@ var _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-    var u="//89.210.57.79/";
+    var u="https://davidvdanos.ddns.net:8443/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
